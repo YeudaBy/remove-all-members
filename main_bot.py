5be91b8c604554cf8ea0d592d808ab707f8c7ea3
naftali100 <48069590+naftali100@users.noreply.github.com>
2020-10-01 15:46:51 +0300

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -32,7 +32,7 @@ def main(c,m):
     chat = m.chat
     global members_count_kicks
     status_me = chat.get_member("me")
-    if status_me.status == "administrator":
+    if status_me.status in ["administrator","creator"]:
         try:
             members_count = str(chat.members_count)
             c.send_message(chat.id,TEXT_STARTED.format(members_count))
